e6200c78fef3e315bdc34fb88561eceb7b8a3532
Jacqueline Abdo <jfabdo@gmail.com>
2020-08-29 16:35:23 -0600

    Moved UnderConstruction to own server

--- underConstruction.go
+++ underConstruction.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"./pages"
 )
 
 func main() {
-	http.HandleFunc("/", pages.HandleSe)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
